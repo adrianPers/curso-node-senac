b354a79a364be6766957fdcf460f1c939b37524b
Adrian Pereira <pers.adrian@outlook.com>
2025-06-28 11:48:40 -0300

    upload da aula 02

---
+++ aula02/app.js
@@ -0,0 +1,31 @@
+
+// import axios from 'axios'
+// const axios = require('axios')
+
+
+//  GET
+// axios.get('http://localhost:3001/usuraios').then(
+//     response => { console.log(response.data) }
+// ).catch(error => { console.error(error) })
+
+
+// PUT
+// axios.put('http://localhost:3001/usuraios/2', {
+//     nome:"Lucas"
+// }).then(
+//     response => { console.log(response.data) }
+// ).catch(error => { console.error(error) })
+
+// // DELETE
+// axios.delete('http://localhost:3001/usuraios/397d').then(
+//     response => { console.log(response.data) }
+// ).catch(error => { console.error(error) })
+
+// // POST
+// axios.post('http://localhost:3001/usuraios',{
+//     nome:"Carla"
+// }).then(
+//     response => { console.log(response.data) }
+// ).catch(error => { console.error(error) })
+
+
